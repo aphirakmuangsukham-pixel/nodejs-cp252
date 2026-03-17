224851096a3bb914646c4409829892451bbcc461
Aphirak <aphirak@g.swu.ac.th>
2026-03-17 05:48:07 +0100

    jestway project

---
+++ jestway/test/models.userData.test.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+const UserData = require('../models/userData');
+
+// Count method
+describe('Count user', () => {
+    test('default user count', () => {
+      testUserData = new UserData();  
+      const userCount = testUserData.getUserCount();
+      expect(userCount).toEqual(4);
+    })
+
+    test('clear user and Count', () => {
+        testUserData = new UserData(); 
+        testUserData.clearUsers()
+        const userCount = testUserData.getUserCount();
+        expect(userCount).toEqual(0);
+    
+    })
+})
+
+// Remove user
+describe('remove user', () => {
+    test('Remove one', () => {
+      testUserData = new UserData();
+      const userCountBefore = testUserData.getUserCount();
+      testUserData.removeOneUser();
+      const userCountAfter = testUserData.getUserCount();
+      const difUser = userCountBefore - userCountAfter;
+      expect(difUser).toEqual(1);
+    })
+})
